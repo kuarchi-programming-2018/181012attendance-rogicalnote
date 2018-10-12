aef1e356f93a57d7115cda9ef1a4ac10df790874
rogicalnote <43873925+rogicalnote@users.noreply.github.com>
2018-10-12 17:59:46 +0900

    あｒｇふぃｂ

--- fibonacci.py
+++ fibonacci.py
@@ -5,10 +5,10 @@ Created on Fri Oct 12 16:57:19 2018
 @author: Takuto
 """
 def fib(n):
-    if n > 2:
+    if n >= 2:
         return fib(n-1) + fib(n-2)
     else:
         return 1
-print(fib(5))
+print(fib(2018))
 
     
